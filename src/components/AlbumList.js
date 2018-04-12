12460efff5f787b8ef85b004b2140066d068c5c7
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-04-12 16:50:10 +0530

    Create new AlbumList component

---
+++ src/components/AlbumList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import axios from 'axios';
+
+class AlbumList extends Component {
+    render() {
+        return (
+            <View>
+                <Text> AlbumList !! </Text>
+            </View>
+        );
+    }
+}
+
+export default AlbumList;
