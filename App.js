328aa8af312996166cead6ac5f2b9b0794d9b702
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-04-12 15:38:31 +0530

    Renamed header to Header

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/components/header';
+import Header from './src/components/Header';
 
 export default class App extends React.Component {
   render() {
