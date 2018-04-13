159ed3040989c9d1e969e047bfe36a4083eb4c6b
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-04-13 14:25:32 +0530

    Add styling to Card component and used it in AlbumDetail

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const Card = () => {
-    retrun (
-        <View></View>
+const Card = (props) => {
+    return (
+        <View style={ styles.containerStyle }>
+            { props.children }
+        </View>
     );
+};
+
+const styles = {
+    containerStyle: {
+
+        // Border Style
+        borderWidth: 1,
+        borderRadius: 2,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+
+        // Shadow Style
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
+        elevation: 1,
+
+        // Margin
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10,
+    }
 };
 
 export default Card;
